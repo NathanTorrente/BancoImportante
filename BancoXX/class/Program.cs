604f51bfb1c6ca9ea64be43bdcb3ce2357573d9a
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-09-18 16:51:14 -0400

    6º Alteração

--- BancoXX/class/Program.cs
+++ BancoXX/class/Program.cs
@@ -13,22 +13,26 @@ while (true)
     ContaEstudante estu = new ContaEstudante(931323232, "1404-4", "Usuário Feliz", 2000,5000, "364.768.160-12", "Instituto Federal (IFRO)");
 
     int esco = Convert.ToInt32(Console.ReadLine());
-    if (esco == 1)
+    if (esco == 1) // Empresarial
     {
  
         Console.WriteLine("\nEscolha uma das funções:");
         Console.WriteLine("1- Fazer Saque.");
         Console.WriteLine("2- Fazer Empréstico\n");
     
-
-        if (esco == 1)
+        int escolha = Convert.ToInt32(Console.ReadLine());
+        if (escolha == 1)
         {
             Console.Write("\nDigite o valor para o Saque: ");
             int valo = Convert.ToInt32(Console.ReadLine());
             empre.Sacar(valo);
+            Console.WriteLine($"Foi Sacado com sucesso: {valo}");
+      
         }
+  
     }
-    else if (esco == 2)
+
+    else if (esco == 2) // Estudante
     {
         Console.WriteLine("\nEscolha uma das funções:\n");
         Console.WriteLine("1- Fazer Saque.");
@@ -39,12 +43,14 @@ while (true)
             Console.Write("\nDigite o valor para o Saque: ");
             int valosaq = Convert.ToInt32(Console.ReadLine());
             empre.Sacar(valosaq);
+            Console.WriteLine($"Foi Sacado com sucesso: {valosaq}");
         }
         else if (esco == 2)
         {
             Console.WriteLine("\nDigite o valor para o Empréstimo: \n");
             int valoempres = Convert.ToInt32(Console.ReadLine());
             empre.RealizarEmprestimo(valoempres);
+            Console.WriteLine($"Foi realizado empréstimo com sucesso: {valoempres}");
         }
         Console.Clear();
     }
